80c603aa7a220fa859506a64522c996ed67bb86c
Ahmet Çelik <ahmet.celik2605@outlook.com>
2023-02-05 17:17:48 +0300

    movement

--- Koru/Assets/Scripts/BlockScript.cs
+++ Koru/Assets/Scripts/BlockScript.cs
@@ -1,22 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BlockScript : MonoBehaviour
 {
+    public bool isWhite;
     public int blockId;
     public int characterNo;
     public bool isFull;
 
     private CharacterManager cm;
+    private GameManager gm;
 
     private void Start()
     {
         cm = GameObject.FindGameObjectWithTag("CharacterManager").GetComponent<CharacterManager>();
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
 
     public void OnMousePressed()
     {
-        cm.UpgradeCharacter(gameObject, blockId, characterNo);
+        //cm.PressedBlock(gameObject, blockId, characterNo);
+        gm.Pressed(gameObject, blockId, characterNo);
+    }
+
+    public void PrepareToMove()
+    {
+        print("prepared");
+    }
+
+    public void Movement(GameObject block)
+    {
+        block.GetComponent<BlockScript>().Change(characterNo);
+
+        transform.GetChild(characterNo - 1).gameObject.SetActive(false);
+        characterNo = 0;
+        isFull = false;
+
+
+        print("moved");
+    }
+
+    public void Change(int _characterNo)
+    {
+        if (isFull)
+        {
+            transform.GetChild(characterNo - 1).gameObject.SetActive(false);
+            transform.GetChild(_characterNo - 1).gameObject.SetActive(true);
+        }
+        else
+        {
+            isFull = true;
+            characterNo = _characterNo;
+            transform.GetChild(_characterNo - 1).gameObject.SetActive(true);
+        }
+
     }
 }
---
+++ Koru/Assets/Scripts/UIScripts/UpgradeButtonScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeButtonScript : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public void UpgradeTheBlock()
+    {
+
+    }
+}
